52f18b820960cf5d6c6ba56c1e8cefc4a77ffa97
Andrey Evenko <andrey5971@ukr.net>
2019-01-07 14:53:44 +0200

    Add comment in Programm class

--- Passwords/Program.cs
+++ Passwords/Program.cs
@@ -14,6 +14,7 @@ namespace Passwords
         [STAThread]
         static void Main()
         {
+            // Some comment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormPassword());
